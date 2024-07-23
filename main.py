c72afaeb73d50920a3365d3c7e2c36d8bb9a89c4
Sonson296 <Hussien.a.m.talha@gmail.com>
2024-07-23 18:28:48 +0300

    chatgpt continue generating 9:16 pm

--- main.py
+++ main.py
@@ -123,7 +123,6 @@ class Browser(QMainWindow):
         browser.page().linkClicked.connect(self.handle_link_clicked)
         browser.setPage(WebEnginePage(browser.page()))
         
-
     def handle_link_clicked(self, url):
         self.create_new_tab(url)
 
